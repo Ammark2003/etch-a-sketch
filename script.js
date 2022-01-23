013ccfc5de478ffac901007a5c5850f27fae99a2
Terroris3r <ammarkhan.k2003@gmail.com>
2022-01-23 22:48:56 +0530

    Added a button which creates new grid with user defined no. of squares

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 function addgrid(height,width){
-    const container = document.querySelector("#container");
+
     const containerInfo= container.getBoundingClientRect();
-    const containerWidth = containerInfo.width;
-    container.style.cssText=`grid-template-columns: repeat(16,auto); grid-template-rows: repeat(16,${containerWidth/width}px);`;
+    const containerHeight = containerInfo.width;
+    container.style.cssText=`grid-template-columns: repeat(${width},auto); grid-template-rows: repeat(${height},auto);`;
     for (let i = 0; i<height**2;i++){
         const gridItem=document.createElement('div');
         gridItem.classList.add('gridItem');
@@ -13,12 +14,29 @@ function addgrid(height,width){
 function changeColor(){
     const gridItems = document.querySelectorAll(".gridItem");
     gridItems.forEach((gridItem)=>{
-        gridItem.addEventListener('mousedown',()=>{
+        gridItem.addEventListener('mouseover',()=>{
             gridItem.style.backgroundColor = "black";
         })
     })
 }
 
-addgrid();
-changeColor();
+function changeGrid() {
+    const container = document.querySelector("#container");
+    container.innerHTML="";
+    let squares;
+    do{
+        squares=prompt("Enter no. of squares on either side (below 100)");
+    }
+    while(squares>100)
+    addgrid(squares,squares);
+    changeColor();
+}
+
+function newGrid() {
+    const button = document.querySelector('#newGrid');
+    button.addEventListener('click', changeGrid);
+}
+
+newGrid();
+
 
