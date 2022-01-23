6d3d799b2f2e67e3df3bf46b30ab3ae4d1d6ae4c
Terroris3r <ammarkhan.k2003@gmail.com>
2022-01-23 23:42:46 +0530

    moving mouse over a grid element now add 10% black color to it

--- script.js
+++ script.js
@@ -15,11 +15,42 @@ function changeColor(){
     const gridItems = document.querySelectorAll(".gridItem");
     gridItems.forEach((gridItem)=>{
         gridItem.addEventListener('mouseover',()=>{
-            gridItem.style.backgroundColor = `rgb(${randomrgb()},${randomrgb()},${randomrgb()})`; //adds random color to the div
-            /*const color=window.getComputedStyle(gridItem).backgroundColor;
-            console.log(color, typeof color);*/
+            //gridItem.style.backgroundColor = `rgb(${randomrgb()},${randomrgb()},${randomrgb()})`; //adds random color to the div
+            const color = addBlack(gridItem);
+            gridItem.style.backgroundColor=color;
         })
     })
+}
+
+function addBlack(gridItem){
+    let color=window.getComputedStyle(gridItem).backgroundColor;
+    let finalColor="rgb(";
+    let start,end;
+    for (let i=0;i<color.length;i++) {
+        if (color[i]=='('){
+            start=i;
+        }
+        else if (color[i]==')'){
+            end=i;
+        }
+    }
+    color = color.slice(start+1,end);
+    const rgb=color.split(',')
+    rgb.forEach ( (value) => {
+        value=value.trim();
+        value=parseInt(value);
+        if (value>25.5){
+            value=value-25.5;
+        }
+        
+        else {
+            value=0;
+            
+        }
+        finalColor+=`${value},`;
+    })
+    finalColor=finalColor.slice(0,finalColor.length-1)+")";
+    return finalColor;
 }
 
 function randomrgb(){
